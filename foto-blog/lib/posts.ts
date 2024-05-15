221d4511c2d5f97f8d10299d3f1dbe36def0c665
Natalia C <46870371+nlunaty@users.noreply.github.com>
2024-05-15 10:01:05 +0100

    challenge

--- foto-blog/lib/posts.ts
+++ foto-blog/lib/posts.ts
@@ -1,9 +1,8 @@
-import path from 'path'
 import fs from 'fs'
 import { Post } from '@/tipos/tiposPost';
 
 export async function cargarPosts(): Promise<Post[]> {
-    const rutaArchivo = path.join(process.cwd(), 'json/posts.json');
+    const rutaArchivo = '../json/posts.json';
     const datosJson = await fs.promises.readFile(rutaArchivo);
     const posts = JSON.parse(datosJson.toString()) as Post[];
   
--- foto-blog/pages/index.tsx
+++ foto-blog/pages/index.tsx
@@ -15,30 +15,28 @@ export async function getStaticProps() {
 
 export default function IndexPage({ datosPosts }: { datosPosts: Post[] }) {
 
-	const [posts, updatePosts] = useState(datosPosts);
-
 	const nuevoComentario = (comentario: string, postId: number) => {
-		updatePosts(posts.map(postActual => {
+		datosPosts = datosPosts.map(postActual => {
 			if (postActual.idPost === postId) {
 				postActual.comentarios.push(comentario);
 			}
 			return postActual;
-		}));
+		})
 	};
 
 	const meGusta = (postId: number) => {
-		updatePosts(posts.map(postActual => {
+		datosPosts = datosPosts.map(postActual => {
 			if (postActual.idPost === postId) {
 				postActual.totalMeGusta += 1;
 			}
 			return postActual;
-		}));
+		})
 	};
 
 	return (
 		<DefaultLayout>
 			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-				{posts.map(post => <PostComponent
+				{datosPosts.map(post => <PostComponent
 					key={post.idPost}
 					{...post}
 					nuevoComentario={nuevoComentario}
